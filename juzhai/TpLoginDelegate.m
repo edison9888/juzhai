bc43ba36fb2ec93953375ce514280f8f3f92636a
wujiajun <wujiajun1020@gmail.com>
2012-08-01 15:01:13 +0800

    login ui change

--- juzhai/TpLoginDelegate.m
+++ juzhai/TpLoginDelegate.m
@@ -41,6 +41,7 @@
         titleLabel.tag = TITLE_VIEW_TAG;
         titleLabel.font = [UIFont fontWithName:DEFAULT_FONT_FAMILY size:13];
         titleLabel.textColor = [UIColor colorWithRed:0.40f green:0.40f blue:0.40f alpha:1.00f];
+        titleLabel.backgroundColor = [UIColor clearColor];
         [cell addSubview:titleLabel];
     }
     
